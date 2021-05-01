f0ba158085a78b4dcbad4f6f7e67d18c4dcffd7b
wafikhsalman <81632023+wafikhsalman@users.noreply.github.com>
2021-05-01 21:35:25 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,61 +1,18 @@
 class script(object):
 
 
-    START_MSG = """ <b> ഹായ് {}
+    START_MSG = """ <b>ഹായ് {}
 ഈ ബോട്ട് തികച്ചും @MGMoviegram ഗ്രൂപ്പിലേക്ക് ആണ്. ഞാൻ ആണ് ആ ഗ്രൂപ്പിൽ സിനിമകൾ നൽകുന്നത്😁
 """
 
 
-    HELP_MSG = """<b>How to use the bot??</b>
-
-<i>
-* Add bot to your group with admin rights.
-
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
+    HELP_MSG = """
+    എന്നെ എന്റെ ഓണർക്ക് മാത്രെമേ കൺട്രോൾ ചെയ്യാൻ പറ്റുകയൊള്ളു...😜
+    MADE BY @JINN_OF_TG  FOR @Wafikh for his movie group............ 
+"""
 
 
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
-    
-⭕️<b>Creater :</b> @TroJanzHEX
-
-⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
-
+    ABOUT_MSG = """
+എന്നെ എന്റെ ഓണർക്ക് മാത്രെമേ കൺട്രോൾ ചെയ്യാൻ പറ്റുകയൊള്ളു...😜
+MADE BY @JINN_OF_TG  FOR @Wafikh for his movie group........... 
 """
